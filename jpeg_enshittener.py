ec88ec874150fc5e54356f34ca0001c536250d05
MNikiforovV <nikiforov.mikhail.v@gmail.com>
2024-11-07 09:55:29 +0700

    It works!

--- jpeg_enshittener.py
+++ jpeg_enshittener.py
@@ -3,3 +3,6 @@ import sys
 
 for arg in sys.argv: 
     print(arg)
+    if arg.endswith('.jpeg') or arg.endswith('.jpg'):
+        with Image.open(arg) as shittening_image:
+            shittening_image.save(arg, "JPEG", quality=250, subsampling=0)
